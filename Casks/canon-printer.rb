9a7f7614d25ed688e3126681492ca25042ffd51b
Christian Albrecht <christian.albrecht@s24.com>
2017-06-30 15:12:20 +0200

    Add missing checksum

--- Casks/canon-printer.rb
+++ Casks/canon-printer.rb
@@ -1,6 +1,6 @@
 cask 'canon-printer' do
   version '10.13.1'
-  sha256 ''
+  sha256 'd686d2205a2077cd261b60040a9e006ff706be8dff3d6abda542814c3f56332c'
 
   url "http://gdlp01.c-wss.com/gds/6/0100008406/03/Mac_UFRII_Ver#{version.no_dots}_00.dmg"
   name 'Canon UFRII Printer Driver'
